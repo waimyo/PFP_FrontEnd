7dbab5a92a3dc5433f0404222480a8dd8a2e4523
yimon-oo <68224135+yimon-oo@users.noreply.github.com>
2022-08-12 09:24:47 +0630

    marquee in menu bar

--- src/views/NavigationDrawer.vue
+++ src/views/NavigationDrawer.vue
@@ -35,6 +35,11 @@
         </h4>
       </v-toolbar-title>
       <v-spacer></v-spacer>
+      
+      <marquee> <h5 class="black--text pl-5">တိုင်းဒေသကြီး/ပြည်နယ်များရှိ ခရိုင်နှင့် မြို့နယ်များ အသစ်ပြင်ဆင်ဖွဲ့စည်းမှုကို Web Portal တွင် Update ပြုလုပ်ထားပါသဖြင့်  ညာဘက်ထောင့်ရှိ <v-icon size="22" v-bind="attrs" v-on="on" class=""
+            >mdi-information-outline</v-icon
+          > နေရာကို နှိပ်၍ Download ရယူနိုင်ပါသည်။ Location File အသစ်ဖြင့် Data Entry ပြုလုပ်ရန်ဖြစ်ပါကြောင်း အသိပေးအပ်ပါသည်။ </h5></marquee>
+      
       <v-menu
         v-model="menu"
         :close-on-content-click="false"
